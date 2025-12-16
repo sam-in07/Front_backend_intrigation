57b45ebbe564082dddce75683ad2bbadd6b86a7b
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-16 19:54:07 +0600

    fetching done

--- client/script.js
+++ client/script.js
@@ -1,14 +1,33 @@
 fetch("http://localhost:3000/users")
-// promice dey
-.then(res => res.json())
+  // promice dey
+  .then((res) => res.json())
 
-.then(data => {
-  console.log(data);
-});
+  .then((data) => {
+    showUserData(data);
+  });
 
-
-function showUserData(users){
-    for(const user of users){
-        console.log(user.id);
-    }
+function showUserData(users) {
+  for (const user of users) {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+<td>
+  <div class="flex items-center gap-3">
+    <div class="avatar">
+      <div class="mask mask-squircle h-12 w-12">
+        <img src="${users.image_url}" alt="Avatar Tailwind CSS Component" />
+      </div>
+    </div>
+    <div>
+      <div class="font-bold">${user.name}</div>
+      <div class="text-sm opacity-50">${user.country}</div>
+    </div>
+  </div>
+</td>
+<td>
+  ${user.job}
+</td>
+<td>${user.favouriteColor}</td>
+`;
+    document.getElementById("users-table").appendChild(tr);
+  }
 }
--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(cors());
 
 let users = [
-    { id: 1, name: "Alice", country: "USA" , Job : "Engineer" , favouriteColor : "Blue" , image_url : "https://avatar.iran.liara.run/public/43" }, 
-    { id: 1, name: "AAAA", country: "BD" , Job : "Engineer" , favouriteColor : "RED" , image_url : "https://avatar.iran.liara.run/public/42" }, 
+    { id: 1, name: "Alice", country: "USA" , job : "Engineer" , favouriteColor : "Blue" , image_url : "https://avatar.iran.liara.run/public/43" }, 
+    { id: 1, name: "AAAA", country: "BD" , job : "Engineer" , favouriteColor : "RED" , image_url : "https://avatar.iran.liara.run/public/42" }, 
 ]
 
 
